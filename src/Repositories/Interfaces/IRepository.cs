ca7c74f55ca33d8beb4f6c791fdbc4c9c85c6ce9
fff <70720317+and777dna@users.noreply.github.com>
2026-09-12 21:54:29 +0200

    repository pattern is added

---
+++ src/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+namespace Messenger.Repositories.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken ct = default);
+    Task<T?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task AddAsync(T entity, CancellationToken ct = default);
+    Task UpdateAsync(T entity, CancellationToken ct = default);
+    Task DeleteAsync(T entity, CancellationToken ct = default);
+}
